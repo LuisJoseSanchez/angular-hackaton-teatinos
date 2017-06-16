fa2ba1ba0ffed01924246b83218559750a15f6d1
Luis José Sánchez <luisjose@espiral.io>
2017-06-16 19:29:12 +0200

    Actualizando aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Esta es la web que hay que capturar.';
 }
